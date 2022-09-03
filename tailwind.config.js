6c29ffe0eef2fa100ce80261a1362fd8080231b0
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-09-03 16:59:01 +0530

    chore: Tiptap configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
     extend: {}
   },
